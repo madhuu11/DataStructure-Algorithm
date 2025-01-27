40b626a11580e93683640299dc526406ba368c56
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2025-01-27 14:30:37 +0530

    Update Basics.java

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -6,6 +6,41 @@ public class Recursion {
 	public static void main(String[] args) {
 //		Print 1 To N Without Loop
 		printNos();
+				
+//		Print HELLO n times without the loop.
+		printHelloNTimes(5);
+		
+//		Print N To 1 Without Loop
+		printNTo1(5);
+		
+//		Sum of first n terms
+		System.out.println(sumOfN(5));
+	}
+
+	private static int sumOfN(int i) {
+		if(i>0) {
+			sum += i;
+			sumOfN(i-1);
+		}
+		return sum;
+//		return i* (i +1)/2;
+	}
+
+	private static void printNTo1(int i) {
+		if(i>0) {
+			System.out.print(i+" ");
+			printNTo1(i-1);
+		}else {
+			return;
+		}
+	}
+
+	private static void printHelloNTimes(int i) {
+		if(i<=0) {
+			return;
+		}
+		System.out.print("HELLO ");
+		printHelloNTimes(i-1);
 	}
 
 	private static void printNos() {
