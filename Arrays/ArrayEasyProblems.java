b03babcdf3c32d53a4dd8f65cc40ad921f6dea4a
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2025-01-27 18:07:39 +0530

    Update ArrayEasyProblems.java

--- Arrays/ArrayEasyProblems.java
+++ Arrays/ArrayEasyProblems.java
@@ -9,6 +9,54 @@ public class ArrayEasyProblems {
 
 //		Largest Element in Array
 		largestElement(intArray);
+
+//		Second largest element in an array
+		secondLargestElement(intArray);
+	}
+
+	private static void secondLargestElement(int[] intArray) {
+		// Brute force approach
+		Arrays.sort(intArray);
+		int n = intArray.length;
+		if(n == 0 || n == 1) {
+			System.out.println(-1);
+		}else {
+			System.out.println(intArray[n-2]);
+		}
+		
+		// Better approach
+		int n = intArray.length;
+		if(n == 0 || n == 1) {
+			System.out.println(-1);
+		}
+		int max = Integer.MIN_VALUE, secondLarge = Integer.MIN_VALUE;
+		for(int i=0;  i<n; i++) {
+			max = Math.max(max, intArray[i]);
+		}
+		for(int i=0;  i<n; i++) {
+			if(intArray[i]>secondLarge && intArray[i]!=max) {
+				secondLarge=intArray[i];
+			}
+		}
+		System.out.println(secondLarge);
+		
+		// Optimal approach
+		int n = intArray.length;
+		if(n < 2) {
+			System.out.println(-1);
+		}
+		int large = Integer.MIN_VALUE, secondLarge = Integer.MIN_VALUE;
+		for(int i=0; i<n; i++) {
+			if(intArray[i] > large) {
+				secondLarge = large;
+				large = intArray[i];
+			}
+			else if(intArray[i] > secondLarge && intArray[i]!=large) {
+				secondLarge = intArray[i];
+			}
+		}
+		System.out.println(secondLarge);
+
 	}
 
 	private static void largestElement(int[] intArray) {
