ef5e5509d5bc325ac3058bfb722a9828e2ae5599
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2025-06-12 22:31:33 +0530

    code using stream API

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -1,12 +1,23 @@
-public class SecondLargest{
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class SecondLargest {
 	
 	public static void main(String[] args) {
 		int[] arr = {2, 6, 3, 18, 11};
-		findSecondLargest(arr);
+		findSecondLargest(arr); //optimal approach
+		findSecondLargestUsingStream(arr); // its time complexity O(nlogn) as it uses .distinct().sorted()
+
+	}
+
+	private static void findSecondLargestUsingStream(int[] arr) {
+		OptionalInt secondLargest = Arrays.stream(arr).distinct().sorted().skip(arr.length-2).findFirst();
+		System.out.println("Second largest number in an array: " +secondLargest);
 	}
 
 	private static void findSecondLargest(int[] arr) {
 		int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+//		if(arr.length < 2) secondLargest = -1; // validate does array contain second element
 		for (int i : arr) {
 			if(i > largest) {
 				secondLargest = largest;
@@ -18,5 +29,4 @@ public class SecondLargest{
 		}
 		System.out.println("Second largest number in an array: " +secondLargest);
 	}
-
 }
