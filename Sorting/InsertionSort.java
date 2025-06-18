0d68aca30d0f1a239f8ee2dbc1876dfb27bfe5a4
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2025-06-18 21:06:54 +0530

    insertion sort algorithm

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+public class InsertionSort {
+	public static void main(String[] args) {
+		int[] arr = { 6, 5, 4, 3 };
+		insertionSort(arr);
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+	private static void insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i], j = i - 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j -= 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+}
